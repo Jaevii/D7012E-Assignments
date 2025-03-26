8555b04c20627e5a7900a1ad7f12d8b2a361ff0d
Ludvig Järvi <jarviludvig@icloud.com>
2025-03-26 08:31:52 +0100

    Update smallest_k_sets.hs

--- Haskell/Assignment 1/smallest_k_sets.hs
+++ Haskell/Assignment 1/smallest_k_sets.hs
@@ -1,9 +1,10 @@
 
 
-threeDifferent :: Int -> Int -> Int-> Bool
-threeDifferent x y z = (x/= y) && (y/=z) && (z/=x)
 
+--smallestKsets :: [Int] -> Int -> --
+
+hello = putStr "A"
 
 main :: IO()
 main = do
-    print (threeDifferent 1 2 1)
+    hello
