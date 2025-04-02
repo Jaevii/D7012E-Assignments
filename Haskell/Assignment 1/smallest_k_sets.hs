4ca2a93f6366a5135b40a3f17d0670d405e3c52e
Ludvig Järvi <98786177+Jaevii@users.noreply.github.com>
2025-04-02 15:37:45 +0200

    Update smallest_k_sets.hs

--- Haskell/Assignment 1/smallest_k_sets.hs
+++ Haskell/Assignment 1/smallest_k_sets.hs
@@ -1,32 +1,32 @@
 -- Ludvig Järvi, ludjrv-1 --
 
--- Define Sublist as a type [(Int, Int, Int, [Int])]
-data Sublist = Sublist {
+-- Define Subset as a type [(Int, Int, Int, [Int])]
+data Subset = Subset {
     sumOfElements :: Int,
     startIndex :: Int,
     endIndex :: Int,
     elements :: [Int]
 } deriving (Show)
 
--- Returns a list of all possible non-empty sublists
-generateSublists :: [Int] -> [Sublist]
-generateSublists xs = [Sublist
+-- Returns a list of all possible non-empty Subsets
+generateSubsets :: [Int] -> [Subset]
+generateSubsets xs = [Subset
         (sumOfList sub) -- sumOfElements
         (i+1) (j+1) -- startIndex, endIndex
         sub -- elements
         | i <- [0..length xs-1], j <- [i..length xs-1] -- i, j
-        , let sub = take (j-i+1) (drop i xs) -- sublist
+        , let sub = take (j-i+1) (drop i xs) -- Subset
     ]
 
--- Computes the sum of a sublist
+-- Computes the sum of a Subset
 sumOfList :: [Int] -> Int
 sumOfList [] = 0
 sumOfList (x:xs) = x + sumOfList xs
 
--- Sorts the list of sublists by the sum of the sublist using Quicksort
-sortSublists :: [Sublist] -> [Sublist]
-sortSublists [] = []
-sortSublists (x:xs) = sortSublists smaller ++ [x] ++ sortSublists larger
+-- Sorts the list of Subsets by the sum of the Subset using Quicksort
+sortSubsets :: [Subset] -> [Subset]
+sortSubsets [] = []
+sortSubsets (x:xs) = sortSubsets smaller ++ [x] ++ sortSubsets larger
     where
         smaller = [a | a <- xs, sumOfElements a <= sumOfElements x]
         larger = [a | a <- xs, sumOfElements a > sumOfElements x]
@@ -36,17 +36,17 @@ printList :: [String] -> String
 printList [] = ""
 printList (x:xs) = x ++ "\n" ++ printList xs
 
--- Stringifies a list of sublists into a list of strings
-printSublists :: [Sublist] -> [String]
-printSublists = map formatSublist 
+-- Stringifies a list of Subsets into a list of strings
+printSubsets :: [Subset] -> [String]
+printSubsets = map formatSubset 
 
--- Stringifies a sublist
-formatSublist :: Sublist -> String
-formatSublist s = show (sumOfElements s) ++ "\t" ++ show (startIndex s) ++ "\t" ++ show (endIndex s) ++ "\t" ++ show (elements s)
+-- Stringifies a Subset
+formatSubset :: Subset -> String
+formatSubset s = show (sumOfElements s) ++ "\t" ++ show (startIndex s) ++ "\t" ++ show (endIndex s) ++ "\t" ++ show (elements s)
 
 -- Function to print the smallest k sets of a list
 smallestKsets :: [Int] -> Int -> IO()
-smallestKsets xs k = putStr ("\nEntire list: " ++ show xs ++ "\n\nsize\ti\tj\tsublist\n" ++ printList (printSublists (take k (sortSublists (generateSublists xs)))) ++ "\n")
+smallestKsets xs k = putStr ("\nEntire list: " ++ show xs ++ "\n\nsize\ti\tj\tSubset\n" ++ printList (printSubsets (take k (sortSubsets (generateSubsets xs)))) ++ "\n")
 
 -- Test cases --
 test1 = [x*(-1)^x | x <- [1..100]] :: [Int] -- k=15
