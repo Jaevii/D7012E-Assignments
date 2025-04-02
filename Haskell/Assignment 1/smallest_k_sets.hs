206389a6d82733051646b5b6cbb22c238f160dba
Ludvig Järvi <98786177+Jaevii@users.noreply.github.com>
2025-04-02 12:35:38 +0200

    Update smallest_k_sets.hs
    simplified the print list function and also added a recursive sum of list function

--- Haskell/Assignment 1/smallest_k_sets.hs
+++ Haskell/Assignment 1/smallest_k_sets.hs
@@ -8,16 +8,20 @@ data Sublist = Sublist {
     elements :: [Int]
 } deriving (Show)
 
-
 -- Returns a list of all possible non-empty sublists
 generateSublists :: [Int] -> [Sublist]
 generateSublists xs = [Sublist
-        (foldr (+) 0 sub) -- sumOfElements
+        (sumOfList sub) -- sumOfElements
         (i+1) (j+1) -- startIndex, endIndex
         sub -- elements
         | i <- [0..length xs-1], j <- [i..length xs-1] -- i, j
         , let sub = take (j-i+1) (drop i xs) -- sublist
     ]
+
+-- Computes the sum of a sublist
+sumOfList :: [Int] -> Int
+sumOfList [] = 0
+sumOfList (x:xs) = x + sumOfList xs
 
 -- Sorts the list of sublists by the sum of the sublist using Quicksort
 sortSublists :: [Sublist] -> [Sublist]
@@ -27,9 +31,14 @@ sortSublists (x:xs) = sortSublists smaller ++ [x] ++ sortSublists larger
         smaller = [a | a <- xs, sumOfElements a <= sumOfElements x]
         larger = [a | a <- xs, sumOfElements a > sumOfElements x]
 
--- Stringifies a list of sublists
-printSublists :: [Sublist] -> String
-printSublists s = unlines (map formatSublist s)
+-- Prints a given list of strings
+printList :: [String] -> String
+printList [] = ""
+printList (x:xs) = x ++ "\n" ++ printList xs
+
+-- Stringifies a list of sublists into a list of strings
+printSublists :: [Sublist] -> [String]
+printSublists = map formatSublist 
 
 -- Stringifies a sublist
 formatSublist :: Sublist -> String
@@ -37,7 +46,7 @@ formatSublist s = show (sumOfElements s) ++ "\t" ++ show (startIndex s) ++ "\t" 
 
 -- Function to print the smallest k sets of a list
 smallestKsets :: [Int] -> Int -> IO()
-smallestKsets xs k = putStr ("\nEntire list: " ++ show xs ++ "\n\nsize\ti\tj\tsublist\n" ++ printSublists (take k (sortSublists (generateSublists xs))) ++ "\n")
+smallestKsets xs k = putStr ("\nEntire list: " ++ show xs ++ "\n\nsize\ti\tj\tsublist\n" ++ printList (printSublists (take k (sortSublists (generateSublists xs)))) ++ "\n")
 
 -- Test cases --
 test1 = [x*(-1)^x | x <- [1..100]] :: [Int] -- k=15
