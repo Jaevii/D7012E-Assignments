347059e8ebe96e87eae3f124322807ea5c40931f
Ludvig Järvi <jarviludvig@icloud.com>
2025-05-06 01:03:16 +0200

    Prolog1 done

--- Prolog/Assignment 1/package_delivery.pl
+++ Prolog/Assignment 1/package_delivery.pl
@@ -36,18 +36,66 @@ Can the robot fetch the package from room r3 and bring it to room r2? If so, how
     10. Pick up the Steel key from r1.
     11. Move into r2.
     12. DONE
+
+State is defined as state(Robot position, Steel Key position, Brass Key position, Package position, Number of held Items)
 */
 
 
 % Move between rooms
+% R1 <-> R2
+action(state(r1, held, BrassKey, Package, Items), move(r1, r2), state(r2, held, BrassKey, Package, Items)).
 
+action(state(r2, held, BrassKey, Package, Items), move(r2, r1), state(r1, held, BrassKey, Package, Items)).
+
+% R1 <-> R3
+action(state(r1, SteelKey, held, Package, Items), move(r1, r3), state(r3, SteelKey, held, Package, Items)).
+
+action(state(r3, SteelKey, held, Package, Items), move(r3, r1), state(r1, SteelKey, held, Package, Items)).
 
 % Pick up items
+% Steel Key
+action(state(Robot, Robot, BrassKey, Package, Items), pickUp(steelkey, Robot), state(Robot, held, BrassKey, Package, NewItems)) :-
+    Items < 2,
+    NewItems is Items + 1.
 
+% Brass Key
+action(state(Robot, SteelKey, Robot, Package, Items), pickUp(brasskey, Robot), state(Robot, SteelKey, held, Package, NewItems)) :-
+    Items < 2,
+    NewItems is Items + 1.
+
+% Package
+action(state(Robot, SteelKey, BrassKey, Robot, Items), pickUp(package, Robot), state(Robot, SteelKey, BrassKey, held, NewItems)) :-
+    Items < 2,
+    NewItems is Items + 1.
 
 % Drop items
+% Steel Key
+action(state(Robot, held, BrassKey, Package, Items), drop(steelkey, Robot), state(Robot, Robot, BrassKey, Package, NewItems)) :-
+    NewItems is Items - 1.
 
+% Brass Key
+action(state(Robot, SteelKey, held, Package, Items), drop(brasskey, Robot), state(Robot, SteelKey, Robot, Package, NewItems)) :-
+    NewItems is Items - 1.
 
+% Package
+action(state(Robot, SteelKey, BrassKey, held, Items), drop(package, Robot), state(Robot, SteelKey, BrassKey, Robot, NewItems)) :-
+    NewItems is Items - 1.
 
+% Solve
+solveR(state(_, _, _, r2, _), _, [done|[]]). % Goal: Robot is in r2 holding the package
+solveR(State, N, [Action|Trace]) :-
+    N > 0,
+    action(State, Action, NewState),
+    N1 is N - 1,
+    solveR(NewState, N1, Trace).
 
-test :- 
+start(T) :- 
+    solveR(state(r1, r1, r2, r3, 0), 12, T),
+    printL(T, 1).
+
+% Print list helper function
+printL([], _).
+printL([H|T], Index) :-
+    format('~t~d~2|. ~w~n', [Index, H]),
+    Next is Index + 1,
+    printL(T, Next).
--- Prolog/Assignment 1/run.sh
+++ Prolog/Assignment 1/run.sh
@@ -4,4 +4,4 @@
 # It assumes that the Prolog interpreter is installed and available in the PATH
 
 # Load the Prolog program
-swipl -q -s package_delivery -g "test, halt."
+swipl -q -s package_delivery.pl -g "start(T), halt."
